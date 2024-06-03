7f4777fc193b8266b445a9d98c2f43a73cd4f6c2
Kuba Wojdas <kubaw9@onet.eu>
2024-06-03 05:04:07 +0200

    Add migration command to the app init, add planet model, add character model

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from .api import blueprint as api_blueprint
 from .extensions import db
+import time
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://admin:admin@db:5432/movie_db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://admin:admin@db/sw_db"
 
     app.register_blueprint(api_blueprint, url_prefix='/api')
     db.init_app(app)
+    time.sleep(10)  # TODO add entrypoint script to wait for database
+
+    with app.app_context():
+        db.create_all()
 
     return app
--- src/api/movies/models.py
+++ src/api/movies/models.py
@@ -1,1 +1,21 @@
 from extensions import db
+
+
+class PlanetModel(db.Model):
+    __tablename__ = "planets"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    diameter = db.Column(db.Integer)
+    population = db.Column(db.Integer)
+    terrain = db.Column(db.String(200))
+    residents = db.Relationship("CharacterModel", back_populates="planet", lazy="dynamic")
+
+
+class CharacterModel(db.Model):
+    __tablename__ = "characters"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    planet_id = db.Column(db.Integer, db.ForeignKey("planets.id"))
+    planet = db.Relationship("PlanetModel", back_populates="characters")
